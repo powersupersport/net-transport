3eec1a1d0984749835cd9f9da307099bcc4993e1
Power Supersport <30773964+powersupersport@users.noreply.github.com>
2019-08-25 13:51:23 +0300

    Circular array added.

---
+++ Src/ClassDev.Networking.Transport.LowLevel/CircularArray.cs
@@ -0,0 +1,44 @@
+﻿public class CircularArray<T>
+{
+	public T [] Array { get; private set; }
+	public int Length { get; private set; }
+	public int CurrentIndex { get; private set; }
+
+	public CircularArray (int length)
+	{
+		Array = new T [length];
+		Length = length;
+	}
+
+	public T this [int index]
+	{
+		get { return Array [GetIndex (index)]; }
+		set { Array [GetIndex (index)] = value; }
+	}
+
+	public int GetIndex (int index)
+	{
+		if (Length == 0)
+			throw new System.ArgumentOutOfRangeException ("There are no spaces for any items in this circular array.");
+
+		if (index == 0)
+			return CurrentIndex;
+
+		index = CurrentIndex + index;
+		index = index % Length;
+
+		if (index < 0)
+			index += Length;
+
+		return index;
+	}
+
+	public void Push (T item)
+	{
+		CurrentIndex += 1;
+		if (CurrentIndex >= Length)
+			CurrentIndex = 0;
+
+		this [0] = item;
+	}
+}
